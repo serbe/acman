a4e1bcd3a3e867d2de7ae36b2e980a3342999245
Discip1e <mkoctuk@gmail.com>
2013-02-26 10:27:42 +0400

    add squid acl files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
     all_acls = []
     all_users = []
     i = 0
-    squid_path = 'd:\acman\etc\squid'
+    squid_path = '/etc/squid'
     Dir.foreach(squid_path) do |fname|
       if fname.include?('.acl')
         all_acls.push(fname.sub('.acl', ''))
@@ -30,10 +30,26 @@ class PagesController < ApplicationController
     all_acls = Group.all
     eos = "\n"
     squid_path = '/etc/squid/'
+
+    Dir.foreach(squid_path) do |fname|
+      if fname.include?('.acl')
+        system '/usr/bin/sudo /bin/rm ' + fname
+      end
+    end
+
     squid = squid_path + 'squid.conf'
     squid_conf = File.readlines(squid)
     new_squid_conf = squid_conf
     all_acls.each do |acl|
+      acl_file = File.open('/tmp/' + acl + '.acl', 'w')
+      User.where(:team => acl).each do |item|
+        acl_file.write(item.ip)
+      end
+      acl_file.close
+      system '/usr/bin/sudo /bin/cp /tmp/' + acl + '.acl ' + squid_path
+      system '/usr/bin/sudo /bin/chown root:root ' + squid_path + acl + '.acl'
+      system '/usr/bin/sudo /bin/rm /tmp/' + acl + '.acl'
+
       unless new_squid_conf.join.include?('acl '+acl.name+'_acman src "'+squid_path+acl.name+'.acl"')
         pos = new_squid_conf.index('acl CONNECT method CONNECT'+eos)
         new_squid_conf.insert(pos+1, 'acl '+acl.name+'_acman src "'+squid_path+acl.name+'.acl"'+eos)
