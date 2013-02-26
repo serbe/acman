3e9cf274ffd0a26aa52e346ab7a0afa4e681e5f3
Discip1e <mkoctuk@gmail.com>
2013-02-26 09:47:52 +0400

    delete squid.conf.new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -45,7 +45,8 @@ class PagesController < ApplicationController
     end
     File.open('/tmp/squid.conf.new', 'w').write(squid_conf.join)
     system '/usr/bin/sudo /bin/cp /tmp/squid.conf.new ' + squid_path + 'squid.conf'
-    system '/usr/bin/sudo /bin/chown root:root /tmp/squid.conf.new'
+    system '/usr/bin/sudo /bin/chown root:root ' + squid_path + 'squid.conf'
+    system '/usr/bin/sudo /bin/rm /tmp/squid.conf.new'
     system '/usr/bin/sudo /usr/sbin/squid -k reconfigure'
     @squid = new_squid_conf
   end
